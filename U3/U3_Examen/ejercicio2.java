414d35749550bf7b33bfda96af6426f7ded51ed5
joseantoniororo19 <55987128+joseantoniororo19@users.noreply.github.com>
2019-11-29 18:58:47 +0100

    Add files via upload

---
+++ U3/U3_Examen/ejercicio2.java
@@ -0,0 +1,25 @@
+package examen;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[]args){
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Vamo a comvertir en Morse:  ");
+        int combinacion = teclado.nextInt();
+        int morse
+        teclado.nextLine();
+
+        convierteEnMorse(combinacion);
+        System.out.println(Arrays.toString(combinacion));
+        System.out.print("Escriba un numero: ");
+        leecombinación(morse);
+
+        while (!Arrays.equals(morse))
+        public String convierteEnMorse
+
+
+        }
+    }
+}
